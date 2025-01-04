ee06346dd6f64a34f896842ecc8058d6c7dd3562
Mark-MDO47 <mdo@ieee.org>
2025-01-04 12:10:46 -0800

    install_configs_in_libraries.sh

---
+++ code/CYDtest/install_configs_in_libraries.sh
@@ -0,0 +1,13 @@
+d=`echo ~/Documents/Arduino/libraries`
+
+# if there is no saved original file, make one
+    mv ${d}/TFT_eSPI/User_Setup.h ${d}/TFT_eSPI/User_Setup_original.h
+fi
+echo Y | cp -p gitignoreRandomNerdFiles/User_Setup.h ${d}/TFT_eSPI
+
+# curiously the lv_conf.h goes in the directory BELOW the lvgl library
+echo Y | cp -p gitignoreRandomNerdFiles/lv_conf.h ${d}
+
+echo "library config files installed in:"
+echo "   ${d}/TFT_eSPI/User_Setup.h"
+echo "   ${d}/lv_conf.h"
