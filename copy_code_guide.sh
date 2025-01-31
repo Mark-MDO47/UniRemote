370824060d3c117203ea1b06c375ee6213b05ceb
Mark-MDO47 <mdo@ieee.org>
2025-01-31 11:48:40 -0800

    update code/README.md

--- copy_code_guide.sh
+++ copy_code_guide.sh
@@ -2,8 +2,11 @@
 # strip this out and also put in code directory
 echo "# Guide to Code" > gitignore.tmp
 echo " " >> gitignore.tmp
-ptrn=`grep -n "^[|].*QRCode.py" README.md | sed "s?:.*?:?" | sed "s?.*?^&?"`
+# ptrn is regular expression to find line of last line of table
+ptrn=`grep -n "^[|].*code/tiny_code_reader" README.md | head -1 | sed "s?:.*?:?" | sed "s?.*?^&?"`
+# num is number of lines in table
 num=`grep -n "^[|]" README.md | grep -n "${ptrn}" | sed "s?:.*??"`
+# this will store the table into gitignore.tmp
 grep "^[|]" README.md | head -${num} >> gitignore.tmp
 echo "diff of new file follows"
 echo "-----------------------------------"
