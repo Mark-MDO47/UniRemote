7eb87b2bf3346c8f4786d91eed58dc3c434e9989
Mark Olson <mdo@ieee.org>
2025-02-20 07:28:25 -0800

    update copy_code_guide.sh and code/README.md

--- copy_code_guide.sh
+++ copy_code_guide.sh
@@ -1,9 +1,9 @@
-# guide to code is first table and ends with QRCode.py
+# guide to code is first table and ends with "--- end of table ---"
 # strip this out and also put in code directory
 echo "# Guide to Code" > gitignore.tmp
 echo " " >> gitignore.tmp
 # ptrn is regular expression to find line of last line of table
-ptrn=`grep -n "^[|].*code/UniRemote " README.md | head -1 | sed "s?:.*?:?" | sed "s?.*?^&?"`
+ptrn=`grep -n "^[|].*[-][-][-] end of table [-][-][-]" README.md | head -1 | sed "s?:.*?:?" | sed "s?.*?^&?"`
 # num is number of lines in table
 num=`grep -n "^[|]" README.md | grep -n "${ptrn}" | sed "s?:.*??"`
 # this will store the table into gitignore.tmp
