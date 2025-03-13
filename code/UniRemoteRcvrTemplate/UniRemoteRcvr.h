64ad6f90842fa5b3d1b177e57376511b8e3d05f7
Mark-MDO47 <Mark-MDO47@users.noreply.github.com>
2025-03-13 08:13:27 -0700

    Update UniRemoteRcvr.h

--- code/UniRemoteRcvrTemplate/UniRemoteRcvr.h
+++ code/UniRemoteRcvrTemplate/UniRemoteRcvr.h
@@ -1,6 +1,23 @@
 /* Author: https://github.com/Mark-MDO47  Feb. 28, 2025
  *  https://github.com/Mark-MDO47/UniRemote
- *
+ */
+
+/*
+   Copyright 2025 Mark Olson
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+/*
  * UniRemoteRcvr - Code for receiving commands from UniRemote
  *
  * Status returns from these routines return an "expanded" esp_err_t code
