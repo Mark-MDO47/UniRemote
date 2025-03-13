cd0561d16919f7c1698f98931bc0fabb067cd100
Mark-MDO47 <Mark-MDO47@users.noreply.github.com>
2025-03-13 07:54:47 -0700

    Update CYDbitBangCalibrate.ino
    Apache License 2.0

--- code/CYDbitBangCalibrate/CYDbitBangCalibrate.ino
+++ code/CYDbitBangCalibrate/CYDbitBangCalibrate.ino
@@ -8,6 +8,22 @@
  *   hardware VSPI interface to talk to the MicroSD card on the ESP32-2432S028R (Cheap Yellow Display or CYD).
  */
 
+/*
+   Copyright 2025 Mark Olson
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+   
 /*  
     Example created by Robert (Chip) Fleming for touchscreen calibration (https://github.com/CF20852/ESP32-2432S028-Touchscreen-Calibration)
     Rui Santos & Sara Santos - Random Nerd Tutorials - https://RandomNerdTutorials.com/touchscreen-calibration/
