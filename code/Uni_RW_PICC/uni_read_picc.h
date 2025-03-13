134bd65484d380ffe96d634290acc5ef4fa19cba
Mark-MDO47 <Mark-MDO47@users.noreply.github.com>
2025-03-13 08:01:56 -0700

    Update uni_read_picc.h
    Apache License

--- code/Uni_RW_PICC/uni_read_picc.h
+++ code/Uni_RW_PICC/uni_read_picc.h
@@ -1,3 +1,23 @@
+/* Author: https://github.com/Mark-MDO47  Dec. 21, 2024
+ *  https://github.com/Mark-MDO47/UniRemote
+ */
+
+/*
+   Copyright 2024, 2025 Mark Olson
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 #ifndef UNI_READ_PICC_H
 #define UNI_READ_PICC_H 1
 
