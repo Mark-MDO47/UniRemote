a94f6b4879156fb855d6dd2b8dfda875efe8fd2b
Mark-MDO47 <Mark-MDO47@users.noreply.github.com>
2025-03-13 08:07:43 -0700

    Update WriteRFID_CYD.ino
    Apache License

--- code/WriteRFID_CYD/WriteRFID_CYD.ino
+++ code/WriteRFID_CYD/WriteRFID_CYD.ino
@@ -1,6 +1,25 @@
 /* Author: https://github.com/Mark-MDO47  Jan 23, 2025
  *  https://github.com/Mark-MDO47/UniRemote
  *
+ */
+
+/*
+   Copyright 2024, 2025 Mark Olson
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+/*
  *  This routine writes and checks command info to PICC (RFID) cards.
  *  It is intended to write PICC cards to be used by UniRemoteCYD.
  *     https://github.com/Mark-MDO47/UniRemote
